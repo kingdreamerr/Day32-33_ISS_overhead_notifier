c495573360cd489111138844dc982582f02f9a11
kingdreamerr <jackmvula458@gmail.com>
2023-03-22 17:11:52 +0200

    Add constants

--- main.py
+++ main.py
@@ -2,3 +2,8 @@ import requests
 from datetime import datetime
 import smtplib
 import time
+
+MY_EMAIL = "froglufka@gmail.com"
+MY_PASSWORD = "learningonestep509"
+MY_LAT = 51.519351
+MY_LONG = -0.129758 
