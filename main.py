4dc6ca8e94d640a28d1afa3dfb82dee1dc02558c
kingdreamerr <jackmvula458@gmail.com>
2023-03-22 17:14:55 +0200

    Add function to check the location of the iss

--- main.py
+++ main.py
@@ -7,3 +7,17 @@ MY_EMAIL = "froglufka@gmail.com"
 MY_PASSWORD = "learningonestep509"
 MY_LAT = 51.519351
 MY_LONG = -0.129758 
+
+
+def is_iss_overhead():
+    response = requests.get(url="http://api.open-notify.org/iss-now.json")
+    response.raise_for_status()
+    data = response.json()["iss_position"]
+
+    iss_latitude = float(data["latitude"])
+    iss_longitude = float(data["longitude"])
+
+    #position of the iss  within +5 or -5 degrees of my position.
+    if MY_LAT-5 <= iss_latitude <= MY_LAT+5 and MY_LONG-5 <= iss_longitude <= MY_LONG+5:
+        return True
+    
