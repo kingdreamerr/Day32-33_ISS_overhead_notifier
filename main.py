be59193f01de2c265a6aec0531800c691fb83d0f
kingdreamerr <jackmvula458@gmail.com>
2023-03-22 17:16:09 +0200

    Add function to check if it is night time

--- main.py
+++ main.py
@@ -20,4 +20,21 @@ def is_iss_overhead():
     #position of the iss  within +5 or -5 degrees of my position.
     if MY_LAT-5 <= iss_latitude <= MY_LAT+5 and MY_LONG-5 <= iss_longitude <= MY_LONG+5:
         return True
-    
+
+
+def is_night():
+    parameters = {
+        "lat": MY_LAT,
+        "lng": MY_LONG,
+        "formatted": 0,
+    }
+    response = requests.get("https://api.sunrise-sunset.org/json", params=parameters)
+    response.raise_for_status()
+    data = response.json()["results"]
+    sunrise = int(data["sunrise"].split("T")[1].split(":")[0])
+    sunset = int(data["sunset"].split("T")[1].split(":")[0])
+
+    time_now = datetime.now().hour
+
+    if time_now >= sunset or time_now <= sunrise:
+        return True
